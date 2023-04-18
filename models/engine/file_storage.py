495be8884bb112f9a7f47e36c656a8a42971af45
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-04-18 10:00:17 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,17 +8,24 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        """returns the dictionary __objects"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns the dictionary __objects filtered by class name"""
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            cls_objects = {}
+            for key, obj in FileStorage.__objects.items():
+                if obj.__class__.__name__ == cls:
+                    cls_objects[key] = obj
+            return cls_objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
+        """Serializes __objects to the JSON file (path: __file_path)"""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
