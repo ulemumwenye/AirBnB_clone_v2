fe01f4572142b4e69d5ad8d14694b6e7a8f9ddff
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-05-23 21:22:04 +0200

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+"""script that starts a Flask web application"""
 
+
+# import Flask class from flask module
+# import render_template for rendering templates to browser
+# fetch data from storage engine
 from flask import Flask, render_template
-from models import *
+
 from models import storage
+
+# create an instance called app of the class by passong the __name__ variable
 app = Flask(__name__)
-
-
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
+def teardown_db(exception=None):
+    """removes the current SQLAlchemy Session
+    """
+    if storage is not None:
+        storage.close()
+
+
+@app.route('/states')
+@app.route('/states/<state_id>')
+def states(state_id=None):
+    """displays a HTML page: inside the tag BODY"""
+    states = storage.all("State")
+    if state_id is None:
+        return render_template('9-states.html', states=states)
+    state = states.get('State.{}'.format(state_id))
+    return render_template('9-states.html', state=state)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(debug=True)
