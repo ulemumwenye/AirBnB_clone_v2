33800902a567e7f612369001009138e297f7ef65
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-05-23 21:43:20 +0200

    Update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+"""script that starts a Flask web application"""
 
+
+# import Flask class from flask module
 from flask import Flask
+
+# create an instance called app of the class by passong the __name__ variable
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index():
-    """returns Hello HBNB!"""
+    """display "Hello HBNB!"
+
+    Returns:
+        str: text on the index page
+    """
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(debug=True)
